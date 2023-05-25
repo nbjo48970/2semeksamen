d9d3201c6d9925b8560a7ac02f4c788cac7cad06
DrWilliz <xander_1999@yahoo.dk>
2023-05-25 10:59:24 +0200

    JS kontakt form HJÆLP

--- javascript/kontakt.js
+++ javascript/kontakt.js
@@ -1,4 +1,4 @@
-
+let formInput = document.getElementById("besked-input");
 function ContactForm(input)
 {
 
@@ -8,30 +8,39 @@ function ContactForm(input)
   let navnInput = document.getElementById("name-input");
   let virksomhedInput = document.getElementById("virksomhed-input");
   let telefonInput = document.getElementById("telefon-input");
+  let beskedError = document.getElementById("besked-error");
+  let nameError = document.getElementById("name-error");
+  let virksomhedError = document.getElementById("virksomhed-error");
+  let telefonError = document.getElementById("telefon-error");
 
 
-watchIt();
+
 
 if (inArray(input.value)) {
   emailError.innerHTML = "Du har en tidligere henvendelse<br> under behandling";
-  emailError.style.color = "rgb(80, 65, 57)";
+  emailError.style.color = "red";
+  emailInput.style.borderColor = "red";
   return false;
 }
 
 if  (navnInput.value=="" || virksomhedInput.value=="" || telefonInput.value==""){
     return false;
 }
+// if (navnInput.value==""){
+//   nameError.innerHTML = "Feltet må ikke være tomt";
+//   nameError.style.color = "red";
+//   nameInput.style.borderColor = "red";
+//   document.contactform.navn.focus();
+// }
+//   else {
+//     // nameError.innerHTML = "";
+//   return true;
+// }
+
+//Alt under a goodiebags
   if (input.value.match(validRegex)){
-    emailError.innerHTML = "Tak for din henvendelse!";
-    emailError.style.color = "green";
-    emailInput.style.borderColor = "rgb(41, 43, 51)";
-    emailInput.value = '';
-    navnInput.value = '';
-    virksomhedInput.value = '';
-    telefonInput.value = '';
-    formInput.value = '';
-    document.contactform.email.focus();
-    return false;
+    emailError.innerHTML = "";
+    emailInput.style.borderColor = "rgb(35, 82, 88)";
   }
     else {
     emailError.innerHTML = "Indtast gyldig e-mailadresse";
@@ -41,6 +50,25 @@ if  (navnInput.value=="" || virksomhedInput.value=="" || telefonInput.value=="")
     return false;
   }
   //sæt funktion her efter test Xander
+  if (watchIt()) {
+    //alt er okay
+    beskedError.innerHTML = "Tak for din henvendelse!";
+    beskedError.style.color = "green";
+    emailInput.value = '';
+    navnInput.value = '';
+    virksomhedInput.value = '';
+    telefonInput.value = '';
+    formInput.value = '';
+    formInput.style.borderColor = "rgb(35, 82, 88)";
+    document.contactform.email.focus();
+    return false;
+  } else {
+    //alt er ikke okay ;( skriv en fejlmeddelse og stop med at submitte
+    beskedError.innerHTML = "Din besked indeholder sensitivt sprog";
+    beskedError.style.color = "red";
+    formInput.style.borderColor = "red";
+    return false;
+  }
 }
 
 let regUsers = ["pede@gmail.com", "thomas@mail.dk", "nicoline@mail.com"];
@@ -52,30 +80,17 @@ function inArray(inVal){
 }
 
 
-let formInput = document.getElementById("besked-input");
-
-
-function watchIt(textaera) {
-
-  // let chatError = document.getElementById("chat-error");
-
-
-
-if (inArray2(document.getElementById("besked-input").value)) {
-  // chatError.innerHTML = "Dit opslag indeholder sprog blokeret af admin";
-  // chatError.style.color = "red";
-  // formInput.style.border = "solid red";
-  alert("BWAKH");
+function watchIt() {
+if (inArray2(formInput.value)) {
   return false;
 }
 else {
   return true;
 }
-
 }
 
-let language = ["XD", "hest", "xD", "Niko", "Schmeter"];
 function inArray2(inVal){
+  let language = ["XD", "hest", "xD", "Niko", "Schmeter"];
     for( var i=0, len=language.length; i < len; i++){
         if (inVal.includes(language[i])) return true;
     }

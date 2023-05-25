1312b82ccf161af346a939c2a80a434777265818
PeterSogaard98 <119843293+PeterSogaard98@users.noreply.github.com>
2023-05-25 22:39:28 +0200

    lang java update

--- javascript/language.js
+++ javascript/language.js
@@ -52,18 +52,29 @@
 //     }
 // }
 
-
-let languageList = ["Dansk", "Engelsk",];
+let languageList = ["Dansk", "English", "Español", "Deutsch", "Français", "Italiano", "Português", "Nederlands",];
 let langOptions = document.getElementById("lang-options");
+let langOptionsTwo = document.getElementById("lang-options-two")
 
 
-for (let i = 0; i < languageList.length; i++) {
+for (let i = 0; i < 4; i++) {
     let li = document.createElement('li');
     li.innerText = languageList[i];
     langOptions.appendChild(li);
     li.style.cursor = "pointer";
-    li.style.margin = "10px";
-    li.style.fontSize = "20px";
+    li.style.margin = "20px";
+    li.style.fontSize = "25px";
+    li.style.fontFamily = "'Montserrat', sans-serif"
+}
+
+for (let i = 4; i < languageList.length; i++) {
+    let li = document.createElement('li');
+    li.innerText = languageList[i];
+    langOptionsTwo.appendChild(li);
+    li.style.cursor = "pointer";
+    li.style.margin = "20px";
+    li.style.fontSize = "25px";
+    li.style.fontFamily = "'Montserrat', sans-serif"
 }
 
 function navToggle() {

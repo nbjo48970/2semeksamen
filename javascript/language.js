2b4990ede1b9156b0747f7da954e8c2f38906658
Macucous <macucous@gmail.com>
2023-05-26 14:19:11 +0200

    updated language switcher

--- javascript/language.js
+++ javascript/language.js
@@ -52,39 +52,52 @@
 //     }
 // }
 
-let languageList = ["Dansk", "English", "Español", "Deutsch", "Français", "Italiano", "Português", "Nederlands",];
+let languageCountriesList = ["Danmark", "United Kingdom", "Sverige", "Norge", "Suomi", "Ísland", "Deutschland", "Nederland", "España", "France"];
+let languageList = ["Dansk", "English", "Svenska", "Norska", "Suomalainen","íslenskur" , "Deutsch", "Nederlands", "Español", "Français"];
+let languageURLs = ["index.html", "index.html", "index.html", "index.html", "index.html", "index.html", "index.html", "index.html", "index.html", "index.html"];
 let langOptions = document.getElementById("lang-options");
-let langOptionsTwo = document.getElementById("lang-options-two")
+// let langOptionsTwo = document.getElementById("lang-options-two")
 
 
-for (let i = 0; i < 4; i++) {
-    let li = document.createElement('li');
-    li.innerText = languageList[i];
-    langOptions.appendChild(li);
-    li.style.cursor = "pointer";
-    li.style.margin = "20px";
-    li.style.fontSize = "25px";
-    li.style.fontFamily = "'Montserrat', sans-serif"
+for (let i = 0; i < languageList.length; i++) {
+    let a = document.createElement('a');
+    a.innerText = languageCountriesList[i];
+    a.href = languageURLs[i];
+    
+    let line = document.createElement('span');
+    line.classList.add('line');
+
+    let subheading = document.createElement('span');
+    subheading.innerText = languageList[i];
+
+    
+    a.appendChild(line);
+    a.appendChild(subheading);
+
+
+    langOptions.appendChild(a);
+
+    // let br = document.createElement('br');
+    // a.appendChild(br);
 }
 
-for (let i = 4; i < languageList.length; i++) {
-    let li = document.createElement('li');
-    li.innerText = languageList[i];
-    langOptionsTwo.appendChild(li);
-    li.style.cursor = "pointer";
-    li.style.margin = "20px";
-    li.style.fontSize = "25px";
-    li.style.fontFamily = "'Montserrat', sans-serif"
-}
+// for (let i = 4; i < languageList.length; i++) {
+//     let li = document.createElement('li');
+//     li.innerText = languageList[i];
+//     langOptionsTwo.appendChild(li);
+//     li.style.cursor = "pointer";
+//     li.style.margin = "20px";
+//     li.style.fontSize = "25px";
+//     li.style.fontFamily = "'Montserrat', sans-serif"
+// }
 
 function navToggle() {
     let langSelect = document.getElementById("lang-bar");
-    langSelect.style.transition = 'width 0.3s linear 0s';
 
-    if (langSelect.style.width === '0vw') {
-        langSelect.style.width = '100vw'        
+    if (langSelect.style.left === '' || langSelect.style.left === '100%' ) {
+        langSelect.style.left = '0'        
     } else {
-        langSelect.style.width = '0vw'
+        langSelect.style.left = '100%'
     }
 }
 
